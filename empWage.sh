d85f648d8574451c7a469811b1b866a6654f76cc
jnk52 <ajienkyanikam52@gmail.com>
2020-05-11 13:44:16 -0700

    uc4

--- empWage.sh
+++ empWage.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -x
-isPartTime=1;
-isFullTime=2;
-EmployeeRatePerHour=20;
-randomCheck=$((RANDOM%3));
-if [ $isPartTime -eq $randomCheck ]
-
-then
-        workingHour=4;
-elif    [ $isFullTime -eq $randomCheck ]
-
-then
-        workingHour=8;
-else
-        workingHour=0;
-fi
-salary=$(($workingHour * $EmployeeRatePerHour));
+ispartTime=1;
+isfullTime=2;
+employeeRatePerHour=20;
+empCheck=$((RANDOM%3))
+case $empCheck in
+        $ispartTime)
+                empHours=4
+                ;;
+        $isfullTime)
+                empHours=8
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+salary=$(($empHours * $employeeRatePerHour));
 echo $salary;
-
