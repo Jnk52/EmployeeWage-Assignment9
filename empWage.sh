f14f2e5c3052b50557e661dc0f7f909f10df8ea8
jnk52 <ajienkyanikam52@gmail.com>
2020-05-11 13:58:14 -0700

    uc7

--- empWage.sh
+++ empWage.sh
@@ -2,30 +2,39 @@
 
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=100;
+MAX_HRS_IN_MONTH=10;
 EMP_RATE_PER_HR=20;
-NUM_WORKING_DAYS=20
+NUM_WORKING_DAYS=20;
 
-
-totalEmpHr=0;
+#VARIABLES
+totalEmpHrs=0;
 totalWorkingDays=0;
 
-while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH &&  $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+function getWorkingHours(){
+        local empCheck=$1
+        case $empCheck in
+                        $IS_FULL_TIME)
+                                empHrs=8
+                                ;;
+                        $IS_PART_TIME)
+                                empHrs=4
+                                ;;
+                                *)
+                                ;;
+                esac
+                echo $empHrs;
+}
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
         ((totalWorkingDays++))
-        empCheck=$((RANDOM%3))
-        case $empCheck in
-                $IS_FULL_TIME)
-                        empHrs=8
-                        ;;
-                $IS_PART_TIME)
-                        empHrs=4
-                        ;;
-        esac
-        totalEmpHrs=$(($totalEmpHrs+$EMP_RATE_PER_HR));
+        empCheck=$((RANDOM%3));
+        empHrs="$(getWorkingHours $empCheck)"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs));
+
 done
-totalSalary=$((totalEmpHrs*$EMP_RATE_PER_HR));
-echo "Total Employee Hours .." $totalEmpHrs
-echo  "Total Employee Salary.."$totalSalary
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+echo "Total Employee hours.." $totalEmpHrs
+echo "Total EmployeeSalary.." $totalSalary
 
 
