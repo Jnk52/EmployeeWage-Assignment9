3e4945e8940004be1e7904068adc0f04a1726ada
jnk52 <ajienkyanikam52@gmail.com>
2020-05-11 12:42:03 -0700

    uc1

--- empWage.sh
+++ empWage.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Welcome to the employee wage"
+
+randomCheck=$((RANDOM%2));
+isPresent=1;
+if [ $isPresent -eq $randomCheck ];
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
+
+
